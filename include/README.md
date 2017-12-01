6c7402a7b7bc5aadb6037df87bbcf11d5b7380d5
Connor Davis <connordavis714@gmail.com>
2017-12-01 15:19:11 +0000

    Working on Sign-Up, not yet functional

---
+++ include/README.md
@@ -0,0 +1,1 @@
+Files in this directory contain connections to the mysql database and use queries to collect/import/modify/remove data from database
--- include/login.php
+++ include/login.php
@@ -7,28 +7,24 @@ define('DB_PASSWORD','hamtastic');
 $con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error()); 
 $db=mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysql_error()); 
 
-/* $ID = $_POST['user']; 
-$Password = $_POST['pass']; */ 
-
 function SignIn() 
 { 
 session_start(); 
-/*starting the session for user profile page*/ 
+//starting the session for user profile page
 if(!empty($_POST['Email'])) 
-/*checking the 'user' name which is from Sign-In.html, is it empty or have some text */
+//checking the user's email address from entry to form in ../hamLogin.html
 { 
-//	$query = mysql_query("SELECT * FROM Clogin where Email = '$_POST[Email]' AND Password = md5('$_POST[Password]')") or die(mysql_error()); 
 	$query = mysql_query("SELECT * FROM Clogin WHERE C_ID = (SELECT Customer_ID FROM Customer WHERE Email = '$_POST[Email]') and Password = md5('$_POST[Password]')") or die(mysql_error()); 
-	$row = mysql_fetch_array($query); //or die(mysql_error()); 
+	$row = mysql_fetch_array($query);
 	if(!empty($row['C_ID']) AND !empty($row['Password'])) 
 	{ 
 		$_SESSION['C_ID'] = $row['Password']; 
-	//	echo "SUCCESSFULLY LOGIN TO USER PROFILE PAGE...";
+	//if login is successful, then proceed to the authenticated user's home page
 		header('Location: ../needsFixing.html'); 
 	} 
 	else 
 	{ 
-		echo "SORRY... YOU ENTERD WRONG ID AND PASSWORD... PLEASE RETRY...";
+	//if login is unsuccessful, then rediredt back to the login page
 		header('Location: ../hamRetryLogin.html'); 
 	} 
 } 
