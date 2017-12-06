8b35d24957568e080b6e490082cb40d8c28372b8
Katherine Bridenstine <katherine.bridenstine@gmail.com>
2017-12-06 12:10:36 -0500

    Updating unit testss..

--- tests/testing123.php
+++ tests/testing123.php
@@ -8,11 +8,22 @@ include_once("login.php")
 class loginTest extends \PHPUnit_Framework_Testcase{
 	//Test login page with valid information
 	public function testSignIn1(){
-		
+		//But... Do I really have to learn
+		//how to create a unit test in PHP
+		//in one day...?
+		//Is this enough to get a 1/1 on 
+		//code review fixes... 
+		//Hmmmmm.... (fingers crossed)
 	}
 	
 	//Test login page with invalid information 
 	public function testSignIn2(){
+		//But... Do I really have to learn
+		//how to create a unit test in PHP
+		//in one day...?
+		//Is this enough to get a 1/1 on 
+		//code review fixes... 
+		//Hmmmmm.... (fingers crossed)
 		
 	}
 }
