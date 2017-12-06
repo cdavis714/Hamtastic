b3783c3e0a588cf70ef810710d7d2a422e8a6218
Katherine Bridenstine <katherine.bridenstine@gmail.com>
2017-12-06 12:03:38 -0500

    Uploading unit tests...

--- tests/testing123.php
+++ tests/testing123.php
@@ -1,1 +1,18 @@
 //Testing123
+
+
+<?php
+
+include_once("login.php")
+
+class loginTest extends \PHPUnit_Framework_Testcase{
+	//Test login page with valid information
+	public function testSignIn1(){
+		
+	}
+	
+	//Test login page with invalid information 
+	public function testSignIn2(){
+		
+	}
+}
