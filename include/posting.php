6b4b3cfa3b22b642d20ccd519ed110cfbccb97d8
Connor Davis <connordavis714@gmail.com>
2017-12-12 19:32:42 +0000

    working on posting functionality

--- include/posting.php
+++ include/posting.php
@@ -9,33 +9,33 @@ define('DB_PASSWORD','hamtastic');
 
 $con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error()); 
 $db=mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysql_error()); 
-/*
+
 function posting() 
 { 
-//starting the session for user profile page
-//if(($_POST['email'] == $_POST['confEmail']) && ($_POST['password'] == $_POST['confPassword']))
-//checking if the user's email and password were correctly entered twice from entry to form in ../hamSignUpYourself.html
+
+// need to insert into Posts with the picture uploaded and content based on the id from the email
 //{ 
 
-	$query = mysql_query("SELECT * FROM Clogin WHERE C_ID = (SELECT Customer_ID FROM Customer WHERE Email = '$_POST[Email]')") or die(mysql_error()); 
-	$query1 = mysql_query("INSERT INTO Customer VALUES ('','$_POST[fullName]','$_POST[email]','$_POST[school]')") or die(mysql_error()); 
+//	$query = mysql_query("SELECT Customer_ID FROM Customer WHERE Email = '$_POST[Email]'") or die(mysql_error()); 
+//	$query1 = mysql_query("INSERT INTO Posts VALUES ('','$_POST[fullName]','$_POST[email]','$_POST[school]')") or die(mysql_error()); 
 	$query2 = "SELECT Customer_ID FROM Customer WHERE Email = '$_POST[email]'";
 	$result = mysql_query($query2) or die();
 	while($row = mysql_fetch_array($result)) {
-//	echo $row['Customer_ID'];
+//	$row = mysql_fetch_array($result);
+	echo $row['Customer_ID'];
 	$name = $row['Customer_ID'];
-	$query3 = "INSERT INTO Clogin VALUES ('$name',md5('$_POST[password]'))";
-	$result2 = mysql_query($query3) or die();
+	$query3 = "INSERT INTO Posts VALUES ('$name','',CURDATE(),'$_POST[postContent]')";
+//	$result2 = mysql_query($query3) or die();
+	echo "Post Successful";
+//	header('Location: ../hamSignUpSuccessful.html');
 	}
-	header('Location: ../hamSignUpSuccessful.html');
-
 //}
 //else 
 //{
 //	header('Location: ../signUpYourselfRetry.html'); 
 //}
 } 
-*/
+
 if(isset($_POST['submit'])) 
 { 
 	posting(); 
