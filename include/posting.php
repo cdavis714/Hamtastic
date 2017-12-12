1559071bea876e0bd3c0a1cf3c61145e081cf2d9
cdavis714 <cdavis714@users.noreply.github.com>
2017-12-12 14:56:53 -0500

    again

--- include/posting.php
+++ include/posting.php
@@ -1,3 +1,304 @@
+<!DOCTYPE html>
+<html>
+<style>
+
+/*body { margin:400px auto; width:600px;}*/
+table, th, td {
+	border: 1px solid black;
+	width:1%;
+	white-space:nowrap;
+}
+
+/* setting button style */
+.btn {
+  background: #bfbfbf;
+  background-image: -webkit-linear-gradient(top, #bfbfbf, #dedede);
+  background-image: -moz-linear-gradient(top, #bfbfbf, #dedede);
+  background-image: -ms-linear-gradient(top, #bfbfbf, #dedede);
+  background-image: -o-linear-gradient(top, #bfbfbf, #dedede);
+  background-image: linear-gradient(to bottom, #bfbfbf, #dedede);
+  -webkit-border-radius: 0;
+  -moz-border-radius: 0;
+  border-radius: 0px;
+  -webkit-box-shadow: 2px 2px 5px #666666;
+  -moz-box-shadow: 2px 2px 5px #666666;
+  box-shadow: 2px 2px 5px #666666;
+  font-family: Arial;
+  color: #403c40;
+  font-size: 18px;
+  /*padding: 11px 40px 11px 40px;*/
+  width: 100%;
+  height: 40px;
+  border: solid #a1a1a1 .1px;
+  text-decoration: none;
+}
+
+
+/* setting button style */
+.btn1 {
+  background: #bfbfbf;
+  background-color: rgba(255, 5, 50, .5);
+  
+  background-image: -webkit-linear-gradient(top, #bfbfbf, #dedede);
+  background-image: -moz-linear-gradient(top, #bfbfbf, #dedede);
+  background-image: -ms-linear-gradient(top, #bfbfbf, #dedede);
+  background-image: -o-linear-gradient(top, #bfbfbf, #dedede);
+  background-image: linear-gradient(to bottom, #bfbfbf, #dedede);
+  
+  background:url(../images/homeImage1.png) no-repeat;
+  -webkit-border-radius: 0;
+  -moz-border-radius: 0;
+  border-radius: 0px;
+  -webkit-box-shadow: 2px 2px 5px #666666;
+  -moz-box-shadow: 2px 2px 5px #666666;
+  box-shadow: 2px 2px 5px #666666;
+  font-family: Arial;
+  color: #000000;
+  font-size: 14px;
+  /*padding: 11px 30px 11px 30px;*/
+  width: 51px;
+  height: 51px;
+  border: solid #a1a1a1 .1px;
+  text-decoration: none;
+}
+
+/* setting button style */
+.btn2 {
+  background: #bfbfbf;
+  background-color: rgba(255, 5, 50, .5);
+  
+  background-image: -webkit-linear-gradient(top, #bfbfbf, #dedede);
+  background-image: -moz-linear-gradient(top, #bfbfbf, #dedede);
+  background-image: -ms-linear-gradient(top, #bfbfbf, #dedede);
+  background-image: -o-linear-gradient(top, #bfbfbf, #dedede);
+  background-image: linear-gradient(to bottom, #bfbfbf, #dedede);
+  
+  background:url(../images/postImage1.png) no-repeat;
+  -webkit-border-radius: 0;
+  -moz-border-radius: 0;
+  border-radius: 0px;
+  -webkit-box-shadow: 2px 2px 5px #666666;
+  -moz-box-shadow: 2px 2px 5px #666666;
+  box-shadow: 2px 2px 5px #666666;
+  font-family: Arial;
+  color: #000000;
+  font-size: 14px;
+  /*padding: 11px 30px 11px 30px;*/
+  width: 51px;
+  height: 51px;
+  border: solid #a1a1a1 .1px;
+  text-decoration: none;
+}
+
+.btn3 {
+  background: #bfbfbf;
+  background-color: rgba(255, 5, 50, .5);
+  
+  background-image: -webkit-linear-gradient(top, #bfbfbf, #dedede);
+  background-image: -moz-linear-gradient(top, #bfbfbf, #dedede);
+  background-image: -ms-linear-gradient(top, #bfbfbf, #dedede);
+  background-image: -o-linear-gradient(top, #bfbfbf, #dedede);
+  background-image: linear-gradient(to bottom, #bfbfbf, #dedede);
+  
+  background:url(../images/searchImage1.png) no-repeat;
+  -webkit-border-radius: 0;
+  -moz-border-radius: 0;
+  border-radius: 0px;
+  -webkit-box-shadow: 2px 2px 5px #666666;
+  -moz-box-shadow: 2px 2px 5px #666666;
+  box-shadow: 2px 2px 5px #666666;
+  font-family: Arial;
+  color: #000000;
+  font-size: 14px;
+  /*padding: 11px 30px 11px 30px;*/
+  width: 51px;
+  height: 51px;
+  border: solid #a1a1a1 .1px;
+  text-decoration: none;
+}
+
+.btn4 {
+  background: #bfbfbf;
+  background-color: rgba(255, 5, 50, .5);
+  
+  background-image: -webkit-linear-gradient(top, #bfbfbf, #dedede);
+  background-image: -moz-linear-gradient(top, #bfbfbf, #dedede);
+  background-image: -ms-linear-gradient(top, #bfbfbf, #dedede);
+  background-image: -o-linear-gradient(top, #bfbfbf, #dedede);
+  background-image: linear-gradient(to bottom, #bfbfbf, #dedede);
+  
+  background:url(../images/profileImage1.png) no-repeat;
+  -webkit-border-radius: 0;
+  -moz-border-radius: 0;
+  border-radius: 0px;
+  -webkit-box-shadow: 2px 2px 5px #666666;
+  -moz-box-shadow: 2px 2px 5px #666666;
+  box-shadow: 2px 2px 5px #666666;
+  font-family: Arial;
+  color: #000000;
+  font-size: 14px;
+  /*padding: 11px 30px 11px 30px;*/
+  width: 51px;
+  height: 51px;
+  border: solid #a1a1a1 .1px;
+  text-decoration: none;
+}
+.btn5 {
+  background: #bfbfbf;
+  background-image: -webkit-linear-gradient(top, #bfbfbf, #dedede);
+  background-image: -moz-linear-gradient(top, #bfbfbf, #dedede);
+  background-image: -ms-linear-gradient(top, #bfbfbf, #dedede);
+  background-image: -o-linear-gradient(top, #bfbfbf, #dedede);
+  background-image: linear-gradient(to bottom, #bfbfbf, #dedede);
+  -webkit-border-radius: 0;
+  -moz-border-radius: 0;
+  border-radius: 0px;
+  -webkit-box-shadow: 2px 2px 5px #666666;
+  -moz-box-shadow: 2px 2px 5px #666666;
+  box-shadow: 2px 2px 5px #666666;
+  font-family: Arial;
+  color: #403c40;
+  float:right;
+  font-size: 18px;
+  margin-right: 40px;
+  margin-top: 5px;
+  width: 100px;
+  height: 40px;
+  border: solid #a1a1a1 .1px;
+  text-decoration: none;
+}
+
+.btn:hover, .btn5:hover {
+  background: #bbd5e6;
+  text-decoration: none;
+}
+
+.btn1:hover {
+  background: url(../images/homeImage2.png) no-repeat;
+  text-decoration: none;
+  position:relative;
+}
+
+.btn2:hover {
+  background: url(../images/postImage2.png) no-repeat;
+  text-decoration: none;
+  position:relative;
+}
+
+.btn3:hover {
+  background: url(../images/searchImage2.png) no-repeat;
+  text-decoration: none;
+  position:relative;
+}
+.btn4:hover {
+  background: url(../images/profileImage2.png) no-repeat;
+  text-decoration: none;
+  position:relative;
+}
+td
+{
+    padding:100px 200px 100px 50px;
+}
+
+.headerBar {
+	position: fixed;
+	background:#ffffff;
+	width:100%;
+	top:0;
+	color: #000000; font-family:Arial; letter-spacing:1px;
+}
+
+input { 
+	width: 100%; 
+	margin-bottom: 10px; 
+	background: rgba(255,255,255, 0.3);
+	border: none;
+	outline: none;
+	padding: 10px;
+	font-size: 13px;
+	font-family: Arial;
+	color: #000;
+	text-shadow: 1px 1px 1px rgba(0,0,0,0);
+	border: 1px solid rgba(0,0,0,0.3);
+	border-radius: 4px;
+	box-shadow: inset 0 -5px 45px rgba(100,100,100,0.2), 0 1px 1px rgba(0,0,0,0.2);
+	-webkit-transition: box-shadow .5s ease;
+	-moz-transition: box-shadow .5s ease;
+	-o-transition: box-shadow .5s ease;
+	-ms-transition: box-shadow .5s ease;
+	transition: box-shadow .5s ease;
+}
+input:focus { box-shadow: inset 0 -5px 45px rgba(100,100,100,0.4), 0 1px 1px rgba(255,255,255,0.2); }
+
+a:link {
+    text-decoration: none;
+    color: inherit;
+    display:block;
+}
+a:visited {
+    text-decoration: none;
+    color: inherit;
+    display:block;
+}
+
+#search {
+
+}
+
+#search input[type="text"] {
+    background: url(search-white.png) no-repeat 10px 6px #fbfbfb;
+    border: 1px solid #d1d1d1;
+    font: bold 14px Arial,Helvetica,Sans-serif;
+    color: #000000;
+    width: 200px;
+    padding: 6px 15px 6px 35px;
+    -webkit-border-radius: 20px;
+    -moz-border-radius: 20px;
+    border-radius: 20px;
+    text-shadow: 0 2px 3px rgba(0, 0, 0, 0.1);
+    -webkit-box-shadow: 0 1px 3px rgba(0, 0, 0, 0.15) inset;
+    -moz-box-shadow: 0 1px 3px rgba(0, 0, 0, 0.15) inset;
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.15) inset;
+    -webkit-transition: all 0.7s ease 0s;
+    -moz-transition: all 0.7s ease 0s;
+    -o-transition: all 0.7s ease 0s;
+    transition: all 0.7s ease 0s;
+    }
+
+#search input[type="text"]:focus {
+    width: 200px;
+    }
+
+
+</style>
+
+<head>
+ 
+<div class="headerBar">
+<hr>
+&nbsp&nbsp<button class="btn1"><a href="../mainPage.html">.</a></button>
+&nbsp&nbsp<button class="btn2"><a href="../postPage.html">.</a></button>
+&nbsp&nbsp<button class="btn3"><a href="../searchPage.html">.</a></button>
+&nbsp&nbsp<button class="btn4"><a href="../profilePage.html">.</a></button>
+
+&nbsp&nbsp<button class="btn5"><a href="../hamLoggedOut.html">Log Out</a></button>
+
+
+<hr>
+
+</div>
+</head>
+<body>
+	<br>
+	<br>
+	
+	<center><table>
+	<tr>
+	<td>
+<img src="../images/hamtasticlogo.jpg" alt="Logo">
+</td>
+<td>
+
 <?php
 
 //Not yet functional!
@@ -42,3 +343,11 @@ if(isset($_POST['submit']))
 } 
 
 ?>
+
+</td>
+</tr>
+</table>
+</center>
+
+</body>
+</html>
