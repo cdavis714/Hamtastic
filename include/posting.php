e3fa80d5de160b1181c4c2ca3794e462b1a71fe6
Connor Davis <connordavis714@gmail.com>
2017-12-12 13:58:17 +0000

    Added search functionality

--- include/posting.php
+++ include/posting.php
@@ -9,7 +9,7 @@ define('DB_PASSWORD','hamtastic');
 
 $con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error()); 
 $db=mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysql_error()); 
-
+/*
 function posting() 
 { 
 //starting the session for user profile page
@@ -35,6 +35,7 @@ function posting()
 //	header('Location: ../signUpYourselfRetry.html'); 
 //}
 } 
+*/
 if(isset($_POST['submit'])) 
 { 
 	posting(); 
