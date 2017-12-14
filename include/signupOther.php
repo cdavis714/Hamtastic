40b4a2a9d455e85a32650a9e93a1215b9cde72b3
Connor Davis <connordavis714@gmail.com>
2017-12-14 13:13:49 +0000

    Added functionality for creating other account

---
+++ include/signupOther.php
@@ -0,0 +1,50 @@
+<?php
+ 
+define('DB_HOST', 'localhost'); 
+define('DB_NAME', 'hamtastic'); 
+define('DB_USER','root'); 
+define('DB_PASSWORD','hamtastic'); 
+
+$con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error()); 
+$db=mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysql_error()); 
+
+function AddOtherUser() 
+{ 
+//starting the session for user profile page
+if($_POST['email'] == $_POST['confEmail'])
+//checking if the user's email and password were correctly entered twice from entry to form in ../hamSignUpYourself.html
+{ 
+	$query1 = mysql_query("INSERT INTO Customer VALUES ('','$_POST[athlete]','$_POST[email]','$_POST[school]')") or die(mysql_error()); 
+	$query2 = "SELECT Customer_ID FROM Customer WHERE Email = '$_POST[email]'";
+	$result = mysql_query($query2) or die();
+	while($row = mysql_fetch_array($result)) {
+//	echo $row['Customer_ID'];
+	$name = $row['Customer_ID'];
+	$query3 = "INSERT INTO Clogin VALUES ('$name',md5('hamtastic'))";
+	$result2 = mysql_query($query3) or die();
+	}
+
+$athleteName = $_POST['athlete'];
+$emailAddr = $_POST['email'];
+
+$to = "$emailAddr";
+$subject = "Hamtastic account created for $athleteName";
+$body = "Someone created an account for you with Hamtastic \n\nYour login information is: \n\nEmail: $emailAddr \nTemporary password: hamtastic \n\n\nPlease visit www.hamtastic.net and login to your account\n";
+//$headers = "From: $emailAddr";
+$headers = "From: accounts@hamtastic.net";
+mail($to,$subject,$body,$headers);
+
+	header('Location: ../hamSignUpOtherSuccessfulHome.html');
+
+}
+else 
+{
+	header('Location: ../signUpOtherRetry.html'); 
+}
+} 
+if(isset($_POST['submit'])) 
+{ 
+	AddOtherUser(); 
+} 
+
+?>
